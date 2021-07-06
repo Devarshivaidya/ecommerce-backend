7bf1a3f96f8b386d878489efeb881b41587ef74a
Devarshivaidya <devarshivaidya2307@gmail.com>
2021-07-06 19:56:06 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cc708e02b35a2b41e1a08346d996cd86'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b051ecb8-69fc-4e60-8fd8-6bb3c7726951'
+};
